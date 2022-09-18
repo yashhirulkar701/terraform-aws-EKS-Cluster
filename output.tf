caa4a6c87471280504f684e82f90f03ccb61818a
Yash Hirulkar <yashhirulkar701@gmail.com>
2022-09-18 10:47:48 +0530

    added disk size

--- output.tf
+++ output.tf
@@ -1,15 +1,19 @@
 output "availability_zones" {
+  description = "to get the availability zones in the region"
   value = aws_subnet.eks_sub.*.availability_zone
 }
 
 output "eks_subnets" {
+   description = "to get the subnet"
   value = aws_eks_cluster.eks_cluster.vpc_config.*.subnet_ids
 }
 
 output "endpoint" {
+   description = "to get the eks cluster endpoint"
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
+   description = "to get the kubeconfig certificate authority data"
   value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
